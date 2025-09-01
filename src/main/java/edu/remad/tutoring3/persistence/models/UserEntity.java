73226f38dbf0a547aa763423c862b9597f612b00
Remy Meier <remad@web.de>
2025-09-01 22:17:13 +0200

    master: UserEntity has proeprties like the keycloak

--- src/main/java/edu/remad/tutoring3/persistence/models/UserEntity.java
+++ src/main/java/edu/remad/tutoring3/persistence/models/UserEntity.java
@@ -1,12 +1,13 @@
 package edu.remad.tutoring3.persistence.models;
 
 import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +15,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "userentity")
+@Table(name = "userentity", uniqueConstraints = { @UniqueConstraint(columnNames = "preferred_username"),
+		@UniqueConstraint(columnNames = "email"), @UniqueConstraint(columnNames = "sub") })
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
@@ -23,42 +25,38 @@ import lombok.Setter;
 public class UserEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Column(unique = true)
+	private String userId;
 
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.USERNAME_REGEX)
-	private String username;
+	@NotNull
+	@Column(name = "name", unique = true)
+	private String name;
 
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.EMAIL_REGEX)
+	@NotNull
+	@Column(name = "email")
 	private String email;
 
-	private Boolean enabled;
+	@NotNull
+	@Column(name = "email_verified")
+	private Boolean emailVerified;
 
-//	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-//	@JoinColumn(name = "user_id")
-//	private List<TokenEntity> tokens;
+	@NotNull
+	@Column(name = "given_name")
+	private String givenName;
+
+	@NotNull
+	@Column(name = "family_name")
+	private String familyName;
+
+	@NotNull
+	@Column(name = "preferred_username")
+	private String preferredUsername;
 	
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.FIRST_NAME_REGEX)
-	private String firstName;
-	
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.LAST_NAME_REGEX)
-	private String lastName;
-	
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.GENDER_REGEX)
-	private String gender;
-	
-//	@NotBlank
-//	@Pattern(regexp = RegexAppConstants.CELL_PHONE_REGEX)
-	private String cellPhone;
-	
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//	private List<AddressEntity> addresses;
-	
+	@NotNull
+	@Column(name = "sub")
+	private String sub;
+
+	@NotNull
 	@Column(name = "creation_date", columnDefinition = "TIMESTAMP")
 	private LocalDateTime creationDate;
 }
