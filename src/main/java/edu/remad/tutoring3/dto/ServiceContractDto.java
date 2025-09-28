4b666bbeb0a77fd0fb95538618bbe4e426cc39ee
Remy Meier <remad@web.de>
2025-09-28 17:02:57 +0200

    master: constructor for service contract dto to create object from service contract entity

--- src/main/java/edu/remad/tutoring3/dto/ServiceContractDto.java
+++ src/main/java/edu/remad/tutoring3/dto/ServiceContractDto.java
@@ -1,5 +1,6 @@
 package edu.remad.tutoring3.dto;
 
+import edu.remad.tutoring3.persistence.models.ServiceContractEntity;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,4 +23,15 @@ public class ServiceContractDto {
 	private long serviceContractNo;
 	private String serviceContractName;
 	private String serviceContractDescription;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param sc {@link ServiceContractEntity}
+	 */
+	public ServiceContractDto(ServiceContractEntity sc) {
+		setServiceContractNo(sc.getServiceContractNo());
+		setServiceContractName(sc.getServiceContractName());
+		setServiceContractDescription(sc.getServiceContractDescription());
+	}
 }
