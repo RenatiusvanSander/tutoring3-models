c36dcbe93f9c1e3ab3281dc6ca4f8639afb607d8
Remy Meier <remad@web.de>
2025-10-19 21:41:15 +0200

    feature/service-contract-price: setup price entity

---
+++ src/main/java/edu/remad/tutoring3/persistence/models/PriceEntity.java
@@ -0,0 +1,46 @@
+package edu.remad.tutoring3.persistence.models;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * price data
+ * 
+ * @author edu.remad
+ * @since 2025
+ */
+@Entity
+@Table(name = "priceentity")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@Builder
+public class PriceEntity {
+	
+	/**
+	 * the primary key for a price
+	 */
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "price_id", unique = true)
+	private long id;
+	
+	@Column(precision = 16, scale = 2)
+	private BigDecimal price;
+}
